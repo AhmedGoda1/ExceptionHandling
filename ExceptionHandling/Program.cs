6de070053d50356c24099c08f3f754ee1c74bd31
Ahmed Gouda <agoda02@gmail.com>
2023-12-13 01:17:52 +0200

    Update Program.cs

--- ExceptionHandling/Program.cs
+++ ExceptionHandling/Program.cs
@@ -16,79 +16,63 @@ namespace ExceptionHandling
 
         static void BasicExceptionHandling()
         {
-            Console.WriteLine("------------------ 1 -----------------------");
-
             Console.Write("Enter a number: ");
-            // TODO:
-            // Convert the input to an integer using int.Parse().
-            // Use a try-catch block to handle FormatException if the user enters a non-numeric value.
-            // Display an error message in case of an exception.
-            // Output the input if correct
             try
             {
+                // Convert the input to an integer using int.Parse().
                 int number = int.Parse(Console.ReadLine());
-                Console.WriteLine("Your number entered is: "+ number);
+                // Output the input if correct
+                Console.WriteLine("Entered number: " + number);
             }
             catch (FormatException)
             {
-                Console.WriteLine("Please enter a valid number");
+                // Handle FormatException if the user enters a non-numeric value.
+                Console.WriteLine("Error: Please enter a valid number.");
             }
-            Console.WriteLine("------------------- 2 ----------------------");
-
         }
 
         static void MultipleExceptionTypes()
         {
             Console.Write("Enter a number: ");
-            // TODO:
-            // Implement BasicExceptionHandling code with following modification
-            // Include a catch block for OverflowException to handle cases where the number is too large or small for an int.
-            // Display appropriate error messages for different exceptions.
-       
             try
             {
-                int number1 = int.Parse(Console.ReadLine());
-                Console.WriteLine(number1);
+                // Convert the input to an integer using int.Parse().
+                int number = int.Parse(Console.ReadLine());
+                // Output the input if correct
+                Console.WriteLine("Entered number: " + number);
             }
             catch (FormatException)
             {
-                Console.WriteLine("Please enter a valid number");
+                // Handle FormatException if the user enters a non-numeric value.
+                Console.WriteLine("Error: Please enter a valid number.");
             }
-            catch(OverflowException)
+            catch (OverflowException)
             {
-                Console.WriteLine("the number is too large or small for an int");
+                // Handle OverflowException if the number is too large or small for an int.
+                Console.WriteLine("Error: The entered number is too large or too small.");
             }
-
-            Console.WriteLine("------------------ 3 -----------------------");
-
         }
 
         static void FinalyBlockUsage()
         {
             Console.Write("Enter a number: ");
-            // TODO:
-            // Implement BasicExceptionHandling code with following modification
-            // Add a finally block to the program.
-            // Use the finally block to display a message whether an exception was caught or not.
             try
             {
-                int number1 = int.Parse(Console.ReadLine());
-                Console.WriteLine(number1);
+                // Convert the input to an integer using int.Parse().
+                int number = int.Parse(Console.ReadLine());
+                // Output the input if correct
+                Console.WriteLine("Entered number: " + number);
             }
             catch (FormatException)
             {
-                Console.WriteLine("Please enter a valid number");
-            }
-            catch (OverflowException)
-            {
-                Console.WriteLine("the number is too large or small for an int");
+                // Handle FormatException if the user enters a non-numeric value.
+                Console.WriteLine("Error: Please enter a valid number.");
             }
             finally
             {
-                Console.WriteLine("this message is displayed whether an exception was caught or not");
+                // Display a message whether an exception was caught or not.
+                Console.WriteLine("Finally block executed.");
             }
-            Console.WriteLine("------------------- 4 ----------------------");
-
         }
 
         // Class for custom exception type
@@ -99,98 +83,96 @@ namespace ExceptionHandling
 
         static void CustomExceptionClass()
         {
-            Console.Write("Enter a positive number: ");
-            // TODO:
-            // Implement BasicExceptionHandling code with following modification
-            // Modify your number input program to throw NegativeNumberException if the user enters a negative number.
-            // Handle this exception in a separate catch block and display an appropriate message.
-           
+            Console.Write("Enter a number: ");
             try
             {
-                int number2 = int.Parse(Console.ReadLine());
+                // Convert the input to an integer using int.Parse().
+                int number = int.Parse(Console.ReadLine());
 
-                if (number2 < 0)
+                // Throw NegativeNumberException if the user enters a negative number.
+                if (number < 0)
                 {
-                    throw new NegativeNumberException("enter a positive not negative number");
+                    throw new NegativeNumberException("Error: Negative numbers are not allowed.");
                 }
-                Console.WriteLine("thanks, you entered number: " + number2);
-            }
-            catch (NegativeNumberException)
-            {
-                Console.WriteLine("enter a positive not negative number");
-            }
-            Console.WriteLine("------------------- 5 ----------------------");
 
+                // Output the input if correct
+                Console.WriteLine("Entered number: " + number);
+            }
+            catch (FormatException)
+            {
+                // Handle FormatException if the user enters a non-numeric value.
+                Console.WriteLine("Error: Please enter a valid number.");
+            }
+            catch (NegativeNumberException ex)
+            {
+                // Handle NegativeNumberException and display an appropriate message.
+                Console.WriteLine(ex.Message);
+            }
         }
 
         static void ExceptionPropagation()
         {
             Console.Write("Enter a number: ");
-            // TODO:
-            // Implement BasicExceptionHandling code with following modification
-            // Write a function CheckNumber that takes an integer and throws ArgumentOutOfRangeException if the number is greater than 100.
-            // In this function, call CheckNumber inside a try block and handle the exception.
-            static void CheckNumber(int number3)
-            {
-                if (number3 > 100)
-                {
-                    throw new ArgumentOutOfRangeException("this number is greater than 100, please try again");
-                }
-            }
-
             try
             {
+                // Write a function CheckNumber that takes an integer and throws ArgumentOutOfRangeException if the number is greater than 100.
                 int number = int.Parse(Console.ReadLine());
                 CheckNumber(number);
-                Console.WriteLine("You entered: " + number);
+                // Output the input if correct
+                Console.WriteLine("Entered number: " + number);
             }
-            catch(ArgumentOutOfRangeException)
+            catch (FormatException)
             {
-                Console.WriteLine("Error Please enter a number less than 100 ");     
-            }     
-
-            
-
-            Console.WriteLine("------------------- 6 ----------------------");
+                // Handle FormatException if the user enters a non-numeric value.
+                Console.WriteLine("Error: Please enter a valid number.");
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                // Handle ArgumentOutOfRangeException and display an appropriate message.
+                Console.WriteLine(ex.Message);
+            }
         }
 
-        // NOTE: You can implement the CheckNumber here
+        // Function to check if the number is greater than 100
+        static void CheckNumber(int number)
+        {
+            if (number > 100)
+            {
+                throw new ArgumentOutOfRangeException("Error: Number cannot be greater than 100.");
+            }
+        }
 
         static void UsingThrowAndCatch()
         {
             Console.Write("Enter a number: ");
-            // TODO:
-            // Implement BasicExceptionHandling code with following modification
-            // Write a function CheckNumberWithLogging that takes an integer and throws ArgumentOutOfRangeException if the number is greater than 100.
-            // Modify the CheckNumberWithLogging function to log the exception message before throwing it.
-            // In this function, catch the exception in the main program and display the logged message.
+            try
             {
-                static void CheckNumberWithLogging(int number6)
-                {
-                    if (number6 > 100)
-                    {
-                        throw new ArgumentOutOfRangeException(nameof(number6), "Number should not be greater than 100.");
-                    }
-                }
-                try
-                {
-                    int number6 = int.Parse(Console.ReadLine());
-                    CheckNumberWithLogging(number6);
-                    Console.WriteLine("You entered: " + number6);
-                }
-                catch (ArgumentOutOfRangeException ex)
-                {
-                    Console.WriteLine($"Logged Error. Number shouldnt be more than a 100");
-                }
+                // Write a function CheckNumberWithLogging that takes an integer and throws ArgumentOutOfRangeException if the number is greater than 100.
+                int number = int.Parse(Console.ReadLine());
+                CheckNumberWithLogging(number);
+                // Output the input if correct
+                Console.WriteLine("Entered number: " + number);
             }
-
-            
-        
-
-        Console.WriteLine("------------------- Thanks ----------------------");
-
+            catch (FormatException)
+            {
+                // Handle FormatException if the user enters a non-numeric value.
+                Console.WriteLine("Error: Please enter a valid number.");
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                // Catch the exception and display the logged message.
+                Console.WriteLine("Logged Exception: " + ex.Message);
+            }
         }
 
-        // NOTE: You can implement the CheckNumberWithLogging here
+        // Function to check if the number is greater than 100 and log the exception message
+        static void CheckNumberWithLogging(int number)
+        {
+            if (number > 100)
+            {
+                Console.WriteLine("Logging Exception: Number cannot be greater than 100.");
+                throw new ArgumentOutOfRangeException("Error: Number cannot be greater than 100.");
+            }
+        }
     }
 }
